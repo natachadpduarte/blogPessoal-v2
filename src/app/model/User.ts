20be99f54314555811a103fdb370ed715d272903
natachadpduarte <natachadpduarte@gmail.com>
2021-03-23 19:28:03 -0300

    Rotas entrar e cadastrar criadas

---
+++ src/app/model/User.ts
@@ -0,0 +1,11 @@
+import { Post} from './Post';
+
+export class User {
+    public id: number
+    public nome: string
+    public usuario: string
+    public senha: string
+    public foto: string
+    public tipo: string
+    public post: Post[]
+}
