cdcfef1578ffe64ca1e86420efcf466cc3105eb7
natachadpduarte <natachadpduarte@gmail.com>
2021-03-25 23:58:12 -0300

    Crud completo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntrarComponent } from './entrar/entrar.component';
+import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
+import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
 
 
 
@@ -14,8 +16,10 @@ const routes: Routes = [
   { path: 'cadastrar', component: CadastrarComponent },
 
   { path: 'inicio', component: InicioComponent },
-  { path: 'tema', component: TemaComponent }
+  { path: 'tema', component: TemaComponent },
 
+  { path: 'tema-edit/:id', component: TemaEditComponent},
+  { path: 'tema-delete/:id', component: TemaDeleteComponent}
 
 ];
 
